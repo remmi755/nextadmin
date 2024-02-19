7c73895612d8bd41ac16ed0e98fdeb6f7b72f63a
remmi755 <90457721+remmi755@users.noreply.github.com>
2024-02-19 17:44:48 +0200

    add style Homepage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import styles from '../app/ui/homepage/homepage.module.css'
 
 const Homepage = () => {
   return (
-    <div>
-      <h1> Homepage</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}> Homepage</h1>
       <Link href={"/dashboard"}>
-        <h2>Go to Dashboard</h2>
+        <h2 className={styles.link}>Go to Dashboard</h2>
       </Link>
     </div>
   );
--- app/ui/login/loginForm/loginForm.jsx
+++ app/ui/login/loginForm/loginForm.jsx
@@ -13,8 +13,8 @@ const LoginForm = () => {
     <div className={styles.container}>
       <form className={styles.form} action={formAction}>
         <h1>Login</h1>
-        <input type="text" placeholder="username" name="username" />
-        <input type="password" placeholder="password" name="password" />
+        <input type="text" placeholder="username : test" name="username" />
+        <input type="password" placeholder="password : test" name="password" />
         <button>Login</button>
         {state && state}
       </form>
